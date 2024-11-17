7ad22b49c30dfe939252c62c7c2c8215eba87c94
dalejandro <daniel.alejandro@damisis.com>
2024-11-17 22:25:14 +0100

    fixes on css

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,3 +1,12 @@
+import './bootstrap';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+
 import React from "react";
 import ReactDOM from "react-dom";
 import '@fortawesome/fontawesome-free/css/all.min.css';
